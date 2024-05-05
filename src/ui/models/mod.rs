2f9012103aa924a49884077672dddb218cfe7c86
Inaha <sakuovds@gmail.com>
2024-05-05 23:34:51 +0800

    chore: creat path recursively

--- src/ui/models/mod.rs
+++ src/ui/models/mod.rs
@@ -4,7 +4,13 @@ pub use self::settings::Settings;
 pub static SETTINGS: Lazy<Settings> = Lazy::new(Settings::default);
 
 pub static CACHE_PATH: Lazy<std::path::PathBuf> =
-    Lazy::new(|| gtk::glib::user_cache_dir().join("tsukimi"));
+    Lazy::new(|| {
+    let path = gtk::glib::user_cache_dir().join("tsukimi");
+    if !path.exists() {
+        std::fs::create_dir_all(&path).expect("Failed to create directory");
+    }
+    path
+});
 
 pub fn emby_cache_path() -> std::path::PathBuf {
     CACHE_PATH.join(std::env::var("EMBY_NAME").unwrap())
