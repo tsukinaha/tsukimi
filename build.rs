0f53ab131e18047a12f67111fc09afbbe3f111b4
kosette <35268640+Kosette@users.noreply.github.com>
2024-09-18 20:50:38 +0800

    fix locale on Windows

--- build.rs
+++ build.rs
@@ -1,4 +1,3 @@
-#[cfg(target_os = "linux")]
 use std::process::Command;
 
 fn main() {
@@ -8,27 +7,24 @@ fn main() {
         "tsukimi.gresource",
     );
 
-    #[cfg(target_os = "linux")]
-    {
-        let po_file = "po/zh_CN.po";
-        let mo_file = "i18n/locale/zh_CN/LC_MESSAGES/tsukimi.mo";
+    let po_file = "po/zh_CN.po";
+    let mo_file = "i18n/locale/zh_CN/LC_MESSAGES/tsukimi.mo";
 
-        let mo_path = std::path::Path::new(mo_file);
+    let mo_path = std::path::Path::new(mo_file);
 
-        if !mo_path.exists() {
-            std::fs::create_dir_all(mo_path.parent().unwrap()).unwrap();
-        }
+    if !mo_path.exists() {
+        std::fs::create_dir_all(mo_path.parent().unwrap()).unwrap();
+    }
 
-        let status = Command::new("msgfmt")
-            .args([po_file, "-o", mo_file])
-            .status()
-            .expect("Failed to compile po file");
+    let status = Command::new("msgfmt")
+        .args([po_file, "-o", mo_file])
+        .status()
+        .expect("Failed to compile po file");
 
-        if status.success() {
-            println!("OK");
-        } else {
-            println!("FAILED");
-        }
+    if status.success() {
+        println!("OK");
+    } else {
+        println!("FAILED");
     }
 
     #[cfg(windows)]
