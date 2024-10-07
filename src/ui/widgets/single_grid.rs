494287250b4d4b9c370f4e84ea6e52317fd2fd89
tsukinaha <sakuovds@gmail.com>
2024-10-07 23:09:09 +0800

    fix grid sort not match

--- src/ui/widgets/single_grid.rs
+++ src/ui/widgets/single_grid.rs
@@ -158,6 +158,11 @@ pub mod imp {
         #[template_child]
         pub scrolled: TemplateChild<TuViewScrolled>,
 
+        #[template_child]
+        pub ascending: TemplateChild<gtk::ToggleButton>,
+        #[template_child]
+        pub descending: TemplateChild<gtk::ToggleButton>,
+
         #[property(get, set, builder(ListType::default()))]
         pub list_type: Cell<ListType>,
         #[property(get, set = Self::set_view_type, builder(ViewType::default()))]
@@ -206,12 +211,9 @@ pub mod imp {
     #[glib::derived_properties]
     impl ObjectImpl for SingleGrid {
         fn constructed(&self) {
+            self.ascending.set_active(SETTINGS.list_sort_order() == 1);
+            self.dropdown.set_selected(SETTINGS.list_sort_by() as u32);
             self.parent_constructed();
-            self.set_sort_by_and_order(
-                SortBy::from(SETTINGS.list_sort_by()),
-                SortOrder::from(SETTINGS.list_sort_order()),
-            );
-            self.obj().handle_type();
         }
 
         fn signals() -> &'static [Signal] {
@@ -353,6 +355,7 @@ impl SingleGrid {
             7 => "ProductionYear,SortName",
             8 => "DatePlayed,SortName",
             9 => "Runtime,SortName",
+            10 => "DateLastContentAdded,SortName",
             _ => "SortName",
         }
     }
