d1a51d9bddd13166dfe0ad898b4c98998af0e4a4
tsukinaha <sakuovds@gmail.com>
2024-10-14 17:17:52 +0800

    client: enable gzip

--- src/client/client.rs
+++ src/client/client.rs
@@ -12,7 +12,7 @@ use uuid::Uuid;
 use crate::{
     config::{proxy::ReqClient, Account, APP_VERSION},
     ui::{models::emby_cache_path, widgets::single_grid::imp::ListType},
-    utils::{spawn, spawn_tokio},
+    utils::spawn_tokio_without_await,
 };
 
 use once_cell::sync::Lazy;
@@ -53,24 +53,23 @@ pub struct EmbyClient {
     pub server_name: Mutex<String>,
 }
 
+fn generate_emby_authorization(
+    user_id: &str,
+    client: &str,
+    device: &str,
+    device_id: &str,
+    version: &str,
+) -> String {
+    format!(
+        "Emby UserId={},Client={},Device={},DeviceId={},Version={}",
+        user_id, client, device, device_id, version
+    )
+}
+
 impl EmbyClient {
     pub fn default() -> Self {
         let mut headers = reqwest::header::HeaderMap::new();
-        headers.insert("Authorization", HeaderValue::from_static("Emby"));
-        headers.insert("X-Emby-Client", HeaderValue::from_static(&CLIENT_ID));
-        headers.insert(
-            "X-Emby-Device-Name",
-            HeaderValue::from_str(&DEVICE_NAME).unwrap(),
-        );
-        headers.insert(
-            "X-Emby-Device-Id",
-            HeaderValue::from_str(&DEVICE_ID).unwrap(),
-        );
-        headers.insert(
-            "X-Emby-Client-Version",
-            HeaderValue::from_static(APP_VERSION),
-        );
-        headers.insert("X-Emby-Language", HeaderValue::from_static("zh-cn"));
+        headers.insert("Accept-Encoding", HeaderValue::from_static("gzip"));
         Self {
             url: Mutex::new(None),
             client: ReqClient::build(),
@@ -92,18 +91,15 @@ impl EmbyClient {
         self.set_user_access_token(&account.access_token)?;
         self.set_server_name(&account.servername)?;
         crate::ui::provider::set_admin(false);
-        spawn(async move {
-            spawn_tokio(async move {
-                match EMBY_CLIENT.authenticate_admin().await {
-                    Ok(r) => {
-                        if r.policy.is_administrator {
-                            crate::ui::provider::set_admin(true);
-                        }
+        spawn_tokio_without_await(async move {
+            match EMBY_CLIENT.authenticate_admin().await {
+                Ok(r) => {
+                    if r.policy.is_administrator {
+                        crate::ui::provider::set_admin(true);
                     }
-                    Err(e) => warn!("Failed to authenticate as admin: {}", e),
                 }
-            })
-            .await;
+                Err(e) => warn!("Failed to authenticate as admin: {}", e),
+            }
         });
         Ok(())
     }
@@ -135,6 +131,25 @@ impl EmbyClient {
             .lock()
             .map_err(|_| anyhow!("Failed to acquire lock on user_id"))?;
         *user_id_lock = user_id.to_string();
+        self.header_change_user_id(user_id)?;
+        Ok(())
+    }
+
+    pub fn header_change_user_id(&self, user_id: &str) -> Result<()> {
+        let mut headers = self
+            .headers
+            .lock()
+            .map_err(|_| anyhow!("Failed to acquire lock on headers"))?;
+        headers.insert(
+            "x-emby-authorization",
+            HeaderValue::from_str(&generate_emby_authorization(
+                user_id,
+                &CLIENT_ID,
+                &DEVICE_NAME,
+                &DEVICE_ID,
+                &APP_VERSION,
+            ))?,
+        );
         Ok(())
     }
 
