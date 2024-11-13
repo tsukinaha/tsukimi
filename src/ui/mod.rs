41f90fe5c1ab58f05207f06611017ed4b97ad34a
tsukinaha <sakuovds@gmail.com>
2024-11-13 09:50:56 +0800

    bump to 0.17.1
    Signed-off-by: tsukinaha <sakuovds@gmail.com>

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -20,7 +20,7 @@ pub fn build_ui(app: &adw::Application) {
                 .application_name("Tsukimi")
                 .version(crate::config::VERSION)
                 .comments(
-                    "A simple third-party Emby client.\nVersion: tsukimi 0.17.0 \n2024.10.18 20:05",
+                    "A simple third-party Emby client.\nVersion: tsukimi 0.17.1 \n2024.10.18 20:05",
                 )
                 // TRANSLATORS: 'Name <email@domain.com>' or 'Name https://website.example'
                 .translator_credits(gettext("translator-credits"))
