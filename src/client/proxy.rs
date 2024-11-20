bedb353f22a99812325fbc8fa981476be817d1bb
tsukinaha <sakuovds@gmail.com>
2024-11-20 21:25:52 +0800

    Revert: proxy settings
    Signed-off-by: tsukinaha <sakuovds@gmail.com>

--- src/client/proxy.rs
+++ src/client/proxy.rs
@@ -16,6 +16,7 @@ impl ReqClient {
     pub fn build() -> ConcurrencyLimit<Client> {
         let settings = gtk::gio::Settings::new(crate::APP_ID);
 
+        #[cfg(target_os = "linux")]
         let client = reqwest::Client::builder()
             .user_agent(APP_USER_AGENT.to_string())
             .timeout(std::time::Duration::from_secs(10))
@@ -23,8 +24,46 @@ impl ReqClient {
             .build()
             .expect("failed to initialize client");
 
+        #[cfg(target_os = "windows")]
+        let client = reqwest::Client::builder()
+            .user_agent(APP_USER_AGENT.to_string())
+            .timeout(std::time::Duration::from_secs(10))
+            .pool_max_idle_per_host(settings.int("threads") as usize)
+            .proxy(reqwest::Proxy::all(get_proxy_settings().unwrap_or_default()))
+            .build()
+            .expect("failed to initialize client");
+
         tower::ServiceBuilder::new()
             .concurrency_limit(SETTINGS.threads() as usize)
             .service(client)
     }
+}
+
+#[cfg(target_os = "windows")]
+use windows::{
+    core::PCWSTR,
+    Win32::Networking::WinHttp::{
+        WinHttpGetIEProxyConfigForCurrentUser,
+        WINHTTP_CURRENT_USER_IE_PROXY_CONFIG,
+    },
+};
+#[cfg(target_os = "windows")]
+pub fn get_proxy_settings() -> Option<String> {
+    unsafe {
+        let mut proxy_config = WINHTTP_CURRENT_USER_IE_PROXY_CONFIG::default();
+        if WinHttpGetIEProxyConfigForCurrentUser(&mut proxy_config).is_ok()
+            && !proxy_config.lpszProxy.is_null()
+        {
+            let proxy = PCWSTR(proxy_config.lpszProxy.0).to_string().unwrap();
+            return Some(proxy);
+        }
+    }
+    None
+}
+#[cfg(target_os = "linux")]
+pub fn get_proxy_settings() -> Option<String> {
+    use std::env;
+    env::var("http_proxy")
+        .or_else(|_| env::var("https_proxy"))
+        .ok()
 }
