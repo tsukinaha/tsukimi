4d6eed8c275fd11463635fa724334b53e7eb16ce
tsukinaha <sakuovds@gmail.com>
2025-01-16 21:09:38 +0800

    error(mpv): Add description for mpv raw error
    Signed-off-by: tsukinaha <sakuovds@gmail.com>

--- src/client/error.rs
+++ src/client/error.rs
@@ -41,11 +41,43 @@ impl UserFacingError for libmpv2::Error {
                 warn!("MPV ErrorLoadfile: {}", error);
                 format!("ErrorLoadfile: {}", error)
             }
+            Self::Raw(error) => {
+                let string = mpv_error_to_string(*error);
+                warn!("MPV Error: {} ({})", string, error);
+                format!("Error: {} ({})", string, error)
+            }
             _ => {
                 warn!("MPV Error: {}", self);
                 format!("Unknown Error: {}", self)
             }
         }
+    }
+}
+
+fn mpv_error_to_string(error: i32) -> &'static str {
+    match error {
+        0 => "Success",
+        -1 => "Event queue full",
+        -2 => "Out of memory",
+        -3 => "Uninitialized",
+        -4 => "Invalid parameter",
+        -5 => "Option not found",
+        -6 => "Option format",
+        -7 => "Option error",
+        -8 => "Property not found",
+        -9 => "Property format",
+        -10 => "Property unavailable",
+        -11 => "Property error",
+        -12 => "Command",
+        -13 => "Loading failed",
+        -14 => "Audio output init failed",
+        -15 => "Video output init failed",
+        -16 => "Nothing to play",
+        -17 => "Unknown format",
+        -18 => "Unsupported",
+        -19 => "Not implemented",
+        -20 => "Generic",
+        _ => "Unknown",
     }
 }
 
