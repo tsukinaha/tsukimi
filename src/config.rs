9020cbf0b2a7230495b9721bcd71b7ca66112210
tsukinaha <sakuovds@gmail.com>
2025-02-20 20:13:55 +0800

    locale: Use LOCALEDIR passed by building system
    Signed-off-by: tsukinaha <sakuovds@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -1,2 +1,8 @@
-pub static VERSION: &str = "0.19.4";
-pub static GETTEXT_PACKAGE: &str = "tsukimi";
+pub const VERSION: &str = "0.19.4";
+pub const GETTEXT_PACKAGE: &str = "tsukimi";
+
+// If you are using meson, this will be replaced with the correct path.
+// Otherwise, you can set it to the correct path where the locale files are installed.
+//
+// This value is reserved for build.rs.
+pub const LOCALEDIR: &str = "/usr/share/locale";
