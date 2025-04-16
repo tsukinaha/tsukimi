66ee3f441ed99f5d4eced58a510d1786bda11198
tsukinaha <sakuovds@gmail.com>
2025-04-16 21:25:45 +0800

    Bump version to 0.21.0
    Signed-off-by: tsukinaha <sakuovds@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub const VERSION: &str = "0.20.0";
+pub const VERSION: &str = "0.21.0";
 pub const GETTEXT_PACKAGE: &str = "tsukimi";
 
 // If you are using meson, this will be replaced with the correct path.
