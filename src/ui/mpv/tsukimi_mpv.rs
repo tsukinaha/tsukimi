22d9b7ef021a0f75612cf55ae00cbb67a02544e9
amtoaer <amtoaer@gmail.com>
2026-05-07 19:30:31 +0800

    fix: always set start

--- src/ui/mpv/tsukimi_mpv.rs
+++ src/ui/mpv/tsukimi_mpv.rs
@@ -243,9 +243,7 @@ impl TsukimiMPV {
     }
 
     pub fn set_start(&self, start_seconds: f64) {
-        if start_seconds > 0.0 {
-            self.set_property("start", format!("{:.2}", start_seconds));
-        }
+        self.set_property("start", format!("{:.2}", start_seconds));
     }
 
     pub fn set_volume(&self, volume: i64) {
