53ac05b7bc59f5422d30b0846dfcfcab9f4c714e
amtoaer <amtoaer@gmail.com>
2026-05-08 02:14:11 +0800

    use async for jellyfin_cache_path

--- src/ui/models/mod.rs
+++ src/ui/models/mod.rs
@@ -1,21 +1,22 @@
 use once_cell::sync::Lazy;
 pub mod settings;
 pub use self::settings::Settings;
-use crate::client::jellyfin_client::JELLYFIN_CLIENT;
+use crate::{
+    client::jellyfin_client::JELLYFIN_CLIENT,
+    utils::spawn_tokio_blocking,
+};
 pub static SETTINGS: Lazy<Settings> = Lazy::new(Settings::default);
 
-pub static CACHE_PATH: Lazy<std::path::PathBuf> = Lazy::new(|| {
-    let path = gtk::glib::user_cache_dir().join("tsukimi");
-    if !path.exists() {
-        std::fs::create_dir_all(&path).expect("Failed to create directory");
-    }
-    path
-});
+pub static CACHE_PATH: Lazy<std::path::PathBuf> =
+    Lazy::new(|| gtk::glib::user_cache_dir().join("tsukimi"));
 
 pub async fn jellyfin_cache_path() -> std::path::PathBuf {
     let path = CACHE_PATH.join(JELLYFIN_CLIENT.server_name_hash.lock().await.as_str());
-    if !path.exists() {
-        std::fs::create_dir_all(&path).expect("Failed to create directory");
-    }
-    path
+    spawn_tokio_blocking(|| {
+        if !path.exists() {
+            std::fs::create_dir_all(&path).expect("Failed to create directory");
+        }
+        path
+    })
+    .await
 }
