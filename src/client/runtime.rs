3ec01c6fd9bdcc5d3cfa53ec4e07a760e32f00a4
ᴀᴍᴛᴏᴀᴇʀ <amtoaer@gmail.com>
2026-05-20 16:29:42 +0800

    feat: throttle concurrent API requests with semaphore (#591)

--- src/client/runtime.rs
+++ src/client/runtime.rs
@@ -5,13 +5,10 @@ use tokio::runtime::{
     Runtime,
 };
 
-use crate::ui::SETTINGS;
-
 pub fn runtime() -> &'static Runtime {
     static RUNTIME: OnceLock<Runtime> = OnceLock::new();
     RUNTIME.get_or_init(|| {
         runtime::Builder::new_multi_thread()
-            .worker_threads(SETTINGS.threads() as usize)
             .enable_all()
             .build()
             .expect("Failed to create runtime")
