1526314dc72930a0f07fb46fd945bd600b9fbe3d
amtoaer <amtoaer@gmail.com>
2026-05-26 18:19:43 +0800

    refactor: move episode subtitle into top bar

--- src/ui/mpv/page.rs
+++ src/ui/mpv/page.rs
@@ -196,8 +196,6 @@ mod imp {
         #[template_child]
         pub title_label2: TemplateChild<gtk::Label>,
         #[template_child]
-        pub episode_title_box: TemplateChild<gtk::Box>,
-        #[template_child]
         pub speed_spin: TemplateChild<gtk::SpinButton>,
         #[template_child]
         pub volume_spin: TemplateChild<gtk::SpinButton>,
@@ -496,7 +494,7 @@ impl MPVPage {
         self.imp()
             .title_label2
             .set_text(title2.as_deref().unwrap_or_default());
-        self.imp().episode_title_box.set_visible(title2.is_some());
+        self.imp().title_label2.set_visible(title2.is_some());
 
         let media_title = title2
             .map(|t| format!("{title1} - {t}"))
