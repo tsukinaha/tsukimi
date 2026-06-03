7b0acd7b6e80ff0125c5cd5606d165ba61ea6e7e
tsukinaha <sakuovds@gmail.com>
2026-06-03 14:57:39 +0800

    Remove build.rs

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // These variables are passed by Meson, see src/config.rs.
-    // We track them here to ensure Meson and direct Cargo calls can share the
-    // same target directory safely.
-    println!("cargo:rerun-if-env-changed=TSUKIMI_VERSION");
-    println!("cargo:rerun-if-env-changed=TSUKIMI_LOCALEDIR");
-    println!("cargo:rerun-if-env-changed=TSUKIMI_PKGDATADIR");
-}
