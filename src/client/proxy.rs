9eeb19fbe1f48f1ee15020e449d4370cb0d3bf72
tsukinaha <sakuovds@gmail.com>
2026-07-06 19:00:27 +0800

    Set http_proxy in client builder

--- src/client/proxy.rs
+++ src/client/proxy.rs
@@ -1,18 +1,22 @@
-use gtk::prelude::*;
-use reqwest::Client;
+use reqwest::{
+    Client,
+    Proxy,
+};
 
 pub struct ReqClient;
 
 impl ReqClient {
     pub fn build() -> Client {
-        let settings = gtk::gio::Settings::new(crate::APP_ID);
-
-        reqwest::Client::builder()
+        let mut client_builder = reqwest::Client::builder()
             .user_agent(crate::USER_AGENT.as_str())
-            .timeout(std::time::Duration::from_secs(10))
-            .pool_max_idle_per_host(settings.int("threads") as usize)
-            .build()
-            .expect("Failed to initialize client")
+            .timeout(std::time::Duration::from_secs(10));
+
+        if let Some(proxy) = get_proxy_settings() {
+            client_builder =
+                client_builder.proxy(Proxy::all(proxy).expect("Failed to initialize proxy"));
+        }
+
+        client_builder.build().expect("Failed to initialize client")
     }
 }
 
