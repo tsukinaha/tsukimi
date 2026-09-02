07f2254013d810db2ff4eb9efab58d7d1a1ad0e7
Inaha <rin@notalium.co>
2026-09-02 14:20:34 +0800

    Use commit hash as part of version (#760)

--- crates/tsukimi/src/config.rs
+++ crates/tsukimi/src/config.rs
@@ -1,9 +1,6 @@
 pub const GETTEXT_PACKAGE: &str = "tsukimi";
 
-pub const VERSION: &str = match option_env!("TSUKIMI_VERSION") {
-    Some(version) => version,
-    None => env!("CARGO_PKG_VERSION"),
-};
+pub const VERSION: &str = compile_version();
 
 pub const LOCALEDIR: &str = match option_env!("TSUKIMI_LOCALEDIR") {
     Some(path) => path,
@@ -15,6 +12,21 @@ pub const PKGDATADIR: &str = match option_env!("TSUKIMI_PKGDATADIR") {
     None => "/usr/share/tsukimi",
 };
 
-pub fn version() -> &'static str {
+pub const fn compile_version() -> &'static str {
+    const fn fallback(version: Option<&'static str>) -> &'static str {
+        match version {
+            Some(version) => version,
+            None => env!("CARGO_PKG_VERSION"),
+        }
+    }
+
+    if option_env!("TSUKIMI_RELEASE").is_none() {
+        return fallback(option_env!("TSUKIMI_GIT_VERSION"));
+    };
+
+    fallback(option_env!("TSUKIMI_VERSION"))
+}
+
+pub const fn version() -> &'static str {
     VERSION
 }
